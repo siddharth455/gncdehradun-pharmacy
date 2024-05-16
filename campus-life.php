0c9a50518a883f867d1baa5f923e951d3d821a14
suraj <surajk32458@gmail.com>
2024-05-16 17:57:00 +0530

    adding the content of tables list

--- campus-life.php
+++ campus-life.php
@@ -72,7 +72,6 @@
 
             <tr class="heading">
                 <th><b>S.No.</b></th>
-                <th><b>Event Date</b></th>
                 <th><b>Event Name</b></th>
                 <th><b>Category of Event </b></th>
             </tr>
@@ -80,10 +79,168 @@
         <tbody>
             <tr>
                 <th scope="row">1</th>
-                <th scope="row">07-25-2023</th>
-                <td>Research workshop on Systematic Approach to Pharmaceutical Care</td>
-                <td>Skill Development</td>
+                <td>Rangreza 2023</td>
+                <td>Language and communication skills</td>
             </tr>
+            <tr>
+                <th scope="row">2</th>
+                <td>Dushreera 2023</td>
+                <td>Life skills (Yoga, physical fitness, health and hygiene) </td>
+            </tr>
+            <tr>
+                <th scope="row">3</th>
+                <td>Poster Presentation</td>
+                <td>Soft skills</td>
+            </tr>
+            <tr>
+                <th scope="row">4</th>
+                <td>Hospital Visit 2023 - CMI</td>
+                <td>Life skills (Yoga, physical fitness, health and hygiene) </td>
+            </tr>
+            <tr>
+                <th scope="row">5</th>
+                <td>Dengue awareness rally</td>
+                <td>Life skills (Yoga, physical fitness, health and hygiene) </td>
+            </tr>
+            <tr>
+                <th scope="row">6</th>
+                <td>Farewell 2023</td>
+                <td>Language and communication skills</td>
+            </tr>
+            <tr>
+                <th scope="row">7</th>
+                <td>Yoga Day 2023</td>
+                <td>Life skills (Yoga, physical fitness, health and hygiene)</td>
+            </tr>
+            <tr>
+                <th scope="row">8</th>
+                <td>Red Cross Day celebration - rangoli competition</td>
+                <td>Soft skills</td>
+            </tr>
+            <tr>
+                <th scope="row">9</th>
+                <td>Earth Day 2023</td>
+                <td>Life skills (Yoga, physical fitness, health and hygiene)</td>
+            </tr>
+            <tr>
+                <th scope="row">10</th>
+                <td>Hospital Visit - Kailash</td>
+                <td>Life skills (Yoga, physical fitness, health and hygiene)</td>
+            </tr>
+            <tr>
+                <th scope="row">11</th>
+                <td>Industrial Visit Himalayan drug 2023</td>
+                <td>Entrepreneurship</td>
+            </tr>
+            <tr>
+                <th scope="row">12</th>
+                <td>Holi 2022 - Ullash, 2022</td>
+                <td>Soft skills</td>
+            </tr>
+            <tr>
+                <th scope="row">13</th>
+                <td>Janausadhi 2023</td>
+                <td>Entrepreneurship</td>
+            </tr>
+            <tr>
+                <th scope="row">14</th>
+                <td>Holi Milan Smaroh, 2023</td>
+                <td>Language and communication skills</td>
+            </tr>
+            <tr>
+                <th scope="row">15</th>
+                <td>India’s 74th Republic Day celebration</td>
+                <td>Language and communication skills</td>
+            </tr>
+            <tr>
+                <th scope="row">16</th>
+                <td>Seminar on Research on  Evidence Based Medicine</td>
+                <td>Awareness of trends in technology </td>
+            </tr>
+            <tr>
+                <th scope="row">17</th>
+                <td>Cricket and Badminton</td>
+                <td>Life skills (Yoga, physical fitness, health and hygiene)</td>
+            </tr>
+            <tr>
+                <th scope="row">18</th>
+                <td>Chess and carrom competition</td>
+                <td>Life skills (Yoga, physical fitness, health and hygiene)</td>
+            </tr>
+            <tr>
+                <th scope="row">19</th>
+                <td>Doodle making competition</td>
+                <td>Soft skills</td>
+            </tr>
+            <tr>
+                <th scope="row">20</th>
+                <td>Volleyball</td>
+                <td>Life skills (Yoga, physical fitness, health and hygiene)</td>
+            </tr>
+            <tr>
+                <th scope="row">21</th>
+                <td>Facepainting</td>
+                <td>Soft skills</td>
+            </tr>
+            <tr>
+                <th scope="row">22</th>
+                <td>Cultural Activities - Solo Dance, Group Dance, Solo Singing and Group Singing</td>
+                <td>Life skills (Yoga, physical fitness, health and hygiene) </td>
+            </tr>
+            <tr>
+                <th scope="row">23</th>
+                <td>Blood Donation Camp/Sports</td>
+                <td>Life skills (Yoga, physical fitness, health and hygiene) </td>
+            </tr>
+            <tr>
+                <th scope="row">24</th>
+                <td>World pharmacist day</td>
+                <td>Language and communication skills</td>
+            </tr>
+            <tr>
+                <th scope="row">25</th>
+                <td>Vishwakarma divas</td>
+                <td>Entrepreneurship</td>
+            </tr>
+            <tr>
+                <th scope="row">26</th>
+                <td>Teachers' day</td>
+                <td>Language and communication skills</td>
+            </tr>
+            <tr>
+                <th scope="row">27</th>
+                <td>Yoga Day 2022</td>
+                <td>Life skills (Yoga, physical fitness, health and hygiene) </td>
+            </tr>
+            <tr>
+                <th scope="row">28</th>
+                <td>Republic Day</td>
+                <td>Language and communication skills</td>
+            </tr>
+            <tr>
+                <th scope="row">29</th>
+                <td>Makar Sankranti</td>
+                <td>Language and communication skills</td>
+            </tr>
+            <tr>
+                <th scope="row">30</th>
+                <td>Pharmacy Day 2019</td>
+                <td>Life skills (Yoga, physical fitness, health and hygiene) </td>
+            </tr>
+            <tr>
+                <th scope="row">31</th>
+                <td>Farewell 2022</td>
+                <td>Language and communication skills</td>
+            </tr>
+            <tr>
+                <th scope="row">32</th>
+                <td>International Conference 2022</td>
+                <td>Awareness of trends in technology</td>
+            </tr>
+
+
+
+
 
         </tbody>
     </table>
--- career-development.php
+++ career-development.php
@@ -24,17 +24,235 @@
           
             <tr class="heading">
                 <th><b>S.No.</b></th>
-                <th><b>Event Date</b></th>
                 <th><b>Event Name</b></th>
                 <th><b>Category of Event </b></th>
             </tr>
         </thead>
         <tbody>
             <tr>
-            <th scope="row">1</th>
-                <th scope="row">07-25-2023</th>
+                <th scope="row">1</th>
                 <td>Research workshop on Systematic Approach to Pharmaceutical Care</td>
                 <td>Skill Development</td>
+            </tr>
+            <tr>
+                <th scope="row">2</th>
+                <td>Research Seminar on How to Design Future Medicines</td>
+                <td>Skill Development</td>
+            </tr>
+            <tr>
+                <th scope="row">3</th>
+                <td>Research Seminar on Advances in Pharmaceutical Technology</td>
+                <td>Skill Development</td>
+            </tr>
+            <tr>
+                <th scope="row">4</th>
+                <td>Seminar on Introducing Pradhan Mantri Bhartiya Janaushadhi Pariyojana (PMBJK) for Pharma Students</td>
+                <td>Entrepreneurship</td>
+            </tr>
+            <tr>
+                <th scope="row">5</th>
+                <td>Seminar on Introduction to Intellectual Property Rights (IPR)</td>
+                <td>Intellectual Property Rights (IPR)</td>
+            </tr>
+            <tr>
+                <th scope="row">6</th>
+                <td>Seminar on Patent Filing System in Pharmacy</td>
+                <td>Intellectual Property Rights (IPR)</td>
+            </tr>
+            <tr>
+                <th scope="row">7</th>
+                <td>Seminar on Importance of  Pharmaco-economics in Pharma research</td>
+                <td>Entrepreneurship</td>
+            </tr>
+            <tr>
+                <th scope="row">8</th>
+                <td>Research Methodology Workshop on Drug Discovery</td>
+                <td>skill development </td>
+            </tr>
+            <tr>
+                <th scope="row">9</th>
+                <td>Seminar on Research on Evidence Based Medicine</td>
+                <td>skill development </td>
+            </tr>
+            <tr>
+                <th scope="row">10</th>
+                <td>Seminar on Entrepreneurship Development</td>
+                <td>Entrepreneurship</td>
+            </tr>
+            <tr>
+                <th scope="row">11</th>
+                <td>Research Seminar on Advances in Biotechnology</td>
+                <td>skill development </td>
+            </tr>
+            <tr>
+                <th scope="row">12</th>
+                <td>Workshop on Patent Search and Analysis</td>
+                <td>Intellectual Property Rights (IPR)</td>
+            </tr>
+            <tr>
+                <th scope="row">13</th>
+                <td>Webinar on Applications of Research and Development in Agri-Pharma Field</td>
+                <td>Entrepreneurship</td>
+            </tr>
+            <tr>
+                <th scope="row">14</th>
+                <td>Workshop on the Use of Patent Information Systems for the Promotion of Innovation</td>
+                <td>Intellectual Property Rights (IPR)</td>
+            </tr>
+            <tr>
+                <th scope="row">15</th>
+                <td>Seminar on Research Areas in Pharmacy Practice </td>
+                <td>Entrepreneurship</td>
+            </tr>
+            <tr>
+                <th scope="row">16</th>
+                <td>Seminar on Drug Research and development extracted from plants</td>
+                <td>Entrepreneurship</td>
+            </tr>
+            <tr>
+                <th scope="row">17</th>
+                <td>Seminar on How to Start Own Retail Pharma Business </td>
+                <td>Entrepreneurship</td>
+            </tr>
+            <tr>
+                <th scope="row">18</th>
+                <td>Seminar on National and International Intellectual Property Rights (IPR) Policies</td>
+                <td>Intellectual Property Rights (IPR)</td>
+            </tr>
+            <tr>
+                <th scope="row">19</th>
+                <td>Seminar on Research Opportunities in India & Abroad</td>
+                <td>Entrepreneurship</td>
+            </tr>
+            <tr>
+                <th scope="row">20</th>
+                <td>Seminar on Development of Entrepreneurial Skills in Pharmacy Students</td>
+                <td>Entrepreneurship</td>
+            </tr>
+            <tr>
+                <th scope="row">21</th>
+                <td>Seminar on How to Publish Scientific Papers</td>
+                <td>skill development</td>
+            </tr>
+            <tr>
+                <th scope="row">22</th>
+                <td>Seminar on Enterprenurship as career Opportunity in Pharma Sector</td>
+                <td>Entrepreneurship</td>
+            </tr>
+            <tr>
+                <th scope="row">23</th>
+                <td>Seminar on developing Enterpreneurial Skills in Pharmacy Students</td>
+                <td>Entrepreneurship</td>
+            </tr>
+            <tr>
+                <th scope="row">24</th>
+                <td>Seminar on Research Methodology: Research Hypothesis & Research Proposal</td>
+                <td>skill development </td>
+            </tr>
+            <tr>
+                <th scope="row">25</th>
+                <td>Enterpreneurial Session on Communication Skill and Personality Development</td>
+                <td>skill development</td>
+            </tr>
+            <tr>
+                <th scope="row">26</th>
+                <td>Seminar on Guidance on How to start Small Scale Enterprise</td>
+                <td>Entrepreneurship</td>
+            </tr>
+            <tr>
+                <th scope="row">27</th>
+                <td>Research seminar on "Opportunities in pharmaceutical manufacturing"</td>
+                <td>Entrepreneurship</td>
+            </tr>
+            <tr>
+                <th scope="row">28</th>
+                <td>Research Session on Computer application in research Methodology</td>
+                <td>Soft skills</td>
+            </tr>
+            <tr>
+                <th scope="row">29</th>
+                <td>Webinar on Importance of Regulatory Affairs in Pharmaceutical Career</td>
+                <td>Entrepreneurship</td>
+            </tr>
+            <tr>
+                <th scope="row">30</th>
+                <td>Webinar on Innovation & Research Methodology of extractioning phytochemicals from plants</td>
+                <td>Entrepreneurship</td>
+            </tr>
+            <tr>
+                <th scope="row">31</th>
+                <td>Research Methodology Webinar on Drug Transformation</td>
+                <td>Entrepreneurship</td>
+            </tr>
+            <tr>
+                <th scope="row">32</th>
+                <td>Seminar on Early Stage Entrepreneurships for pharma students</td>
+                <td>Entrepreneurship</td>
+            </tr>
+            <tr>
+                <th scope="row">33</th>
+                <td>Seminar for pharmacy students on Converting Innovation into Entrepreneurship</td>
+                <td>Entrepreneurship</td>
+            </tr>
+            <tr>
+                <th scope="row">34</th>
+                <td>Research  Seminar on Pharmacy Career in Regulatory Affairs</td>
+                <td>skill development</td>
+            </tr>
+            <tr>
+                <th scope="row">35</th>
+                <td>Research Seminar on Advances in Pharmaceutical Analysis</td>
+                <td>skill development</td>
+            </tr>
+            <tr>
+                <th scope="row">36</th>
+                <td>Seminar on Medical research and writing</td>
+                <td>Intellectual Property Rights (IPR)</td>
+            </tr>
+            <tr>
+                <th scope="row">37</th>
+                <td>Seminar on proficiency in Scientific Writing</td>
+                <td>Soft skills</td>
+            </tr>
+            <tr>
+                <th scope="row">38</th>
+                <td>Pharma Seminar on IPR and Patent Laws</td>
+                <td>Intellectual Property Rights (IPR)</td>
+            </tr>
+            <tr>
+                <th scope="row">39</th>
+                <td>Seminar on Literature review and protocol preparation</td>
+                <td>Soft skills</td>
+            </tr>
+            <tr>
+                <th scope="row">40</th>
+                <td>Seminar on IPR and patent filing</td>
+                <td>Intellectual Property Rights (IPR)</td>
+            </tr>
+            <tr>
+                <th scope="row">41</th>
+                <td>Research workshop on Leadership and Confidence Building</td>
+                <td>Language and communication skills</td>
+            </tr>
+            <tr>
+                <th scope="row">42</th>
+                <td>Entrepreneurship Orientation Seminar for pharmacy students</td>
+                <td>Entrepreneurship</td>
+            </tr>
+            <tr>
+                <th scope="row">43</th>
+                <td>Seminar on Pharma Innovation and Research Development Program</td>
+                <td>Entrepreneurship</td>
+            </tr>
+            <tr>
+                <th scope="row">44</th>
+                <td>Reseacrh workshop on"Pharmacy: Opportunities & Challenges"</td>
+                <td>Entrepreneurship</td>
+            </tr>
+            <tr>
+                <th scope="row">45</th>
+                <td>Seminar on Research Design execution and publication</td>
+                <td>Soft skills</td>
             </tr>
             
         </tbody>
