db53a233122a6bc59aa9f5d2083262c3f0144ac1
suraj <surajk32458@gmail.com>
2024-05-30 10:45:13 +0530

    changes

--- about.php
+++ about.php
@@ -4,7 +4,7 @@
         <div class="d-table-cell">
             <div class="container">
                 <div class="page-title-content">
-                    <h2>About Us</h2>
+                    <h1>About Us</h1>
                 </div>
             </div>
         </div>
