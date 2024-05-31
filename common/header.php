731cbdc7a7efa80a93497b81cf9ec9fa54bf8d4b
bfitcollege2 <sunil61k83@gmail.com>
2024-05-31 11:28:10 +0530

    update

--- common/header.php
+++ common/header.php
@@ -146,8 +146,16 @@
                                     <li class="nav-item">
                                         <a href="co-peo-pso.php" class="nav-link">CO-PO-PSO</a>
                                     </li>
-                                    <li class="nav-item" style="position: relative;">
-                                        <a href="libraries.php" class="nav-link">Library</a>
+                                    <li class="nav-item">
+                                        <a href="#" class="nav-link dropdown-toggle">Library</a>
+                                        <ul class="dropdown-menu">
+                                            <li class="nav-item">
+                                                <a href="libraries.php" class="nav-link">Library Overview</a>
+                                            </li>
+                                            <li class="nav-item">
+                                                <a href="b-pharmacy.php" class="nav-link">B.Pharm</a>
+                                            </li>
+                                        </ul>
                                     </li>
                                     <li class="nav-item">
                                         <a href="value-added-courses.php" class="nav-link">Value Added Courses</a>
