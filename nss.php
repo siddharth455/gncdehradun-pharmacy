bd133f60bf2db2bdd141a643e0cbaad660553b23
siddharth455 <sharmasiddharth269@gmail.com>
2024-06-01 12:41:19 +0530

    changes in nss page

--- nss.php
+++ nss.php
@@ -89,8 +89,8 @@
                 <p>The scheme was launched in 1969, with the aim of fostering social consciousness, promoting community service, and developing the personality of students through active participation in various social welfare activities.</p>
                 <p>The symbol of the NSS is designed based on ‘Rath wheel’ of Konark, the Sun Temple of Odisha. The wheel signifies the progressive cycle of life. The symbol of NSS Badge contains 24 wheels in the Rath of the Sun Temple of Konark. Each Wheel has eight bars which represents 8 pahars of day. The red colour in the badge indicates that the NSS volunteers are full of blood, i.e. lively, active and spirited. The navy blue color indicates the cosmos of which the NSS is a tiny part ready to contribute its share for the welfare of the mankind.</p>
                 <p>The NSS is implemented as a student-centered program in colleges and universities across the country. It encourages students to engage in community service and participate in activities that address social issues such as literacy, health and hygiene, environmental conservation, rural development, and social justice. The program promotes the idea of empathy, social responsibility, and active citizenship among young people.</p>
-                <p><strong>The NSS Unit of Guru Nanak College of Pharmaceutical was established on March 19, 2020.</strong> This unit is self-financed, and only, 50 students were allotted initially. In line with this, an NSS committee was constituted. A lot of programs have been organized by the NSS unit through various departments in the university on a timely basis. In 2022, a fully government-financed unit was allotted to the university. Now the capacity of the unit is 200 students.</p>
-                <p>NSS activities are carried out through regular camps, 7-day special camping programs, and ongoing projects. During these programs, student volunteers work actively to bring about social change and development.</p>
+                <p><strong>The NSS Unit of Guru Nanak College of Pharmaceutical was established on March 19, 2020.</strong> This unit is self-financed, and only, 30 students were allotted initially. A lot of programs have been organized by the NSS unit on a timely basis.</p>
+                <p>NSS activities are carried out through regular camps and ongoing projects. During these programs, student volunteers work actively to bring about social change and development.</p>
             </div>
             <div class="nss-align">
         <div class="nss-image">
@@ -103,25 +103,25 @@
                 <li><b>2.</b> To comprehend the interdependence among themselves and the various aspects of the community for mutual sustenance and progress.</li>
                 <li><b>3.</b> To develop a comprehensive framework for resolving the various challenges faced by the community in meeting its diverse needs and attaining sustainable growth.</li>
                 <li><b>4.</b> To prepare themselves for the service of the nation in the future through analyzing the macro and micro ecosystem of society.</li>
-                <li><b>5.</b>To develop a civic sense of service and responsibility among themselves.</li>
-                <li><b>6.</b>To acquire democratic and humanistic values.</li>
+                <li><b>5.</b> To develop a civic sense of service and responsibility among themselves.</li>
+                <li><b>6.</b> To acquire democratic and humanistic values.</li>
             </ul>
-</div>
+
         </div>
     </div>
         </section>
        <div class="nss-standard">
 <h2>Standard Operating Procedure of NSS:</h2>
 <ul class="list-unstyled p-0 list-icon-4 ps-4 mt-3">
-    <li>Students who wish to join as an NSS Volunteer in the Unit are asked to fill a Form which is kept as an official record and documentation with the University.</li>
+    <li>Students who wish to join as an NSS Volunteer in the Unit are asked to fill a Form which is kept as an official record and documentation with the college.</li>
     <li>The volunteers from among the potential enthusiasts are selected after a screening interview process.</li>
     <li>The volunteers will be required to attend the meetings and accordingly make themselves available for service through the various programs and activities organized by the NSS unit.</li>
     <li>The student volunteers will play an active role in the planning and implementation of such social activities as would improve the quality of life of the economically challenged sections of the community.</li>
     <li>The volunteers will encourage and propagate the dignity of labour at all levels and through all means.</li>
     <li>The volunteers will continually and gradually engage themselves into such activities as would develop the physical and moral health of the self and the community at large.</li>
-    <li>The NSS unit as per the academic calendar of the university organize and conduct day camps keeping in view the needs of the society and programs of the unit.</li>
-    <li>The NSS students/volunteers shall at all times adhere to the best practices and values of the university.</li>
-    <li>The NSS unit in the university shall take all due measures to ensure that any and all projects undertaken and/or initiated by the university are successfully completed.</li>
+    <li>The NSS unit as per the academic calendar of the college organize and conduct day camps keeping in view the needs of the society.</li>
+    <li>The NSS students/volunteers shall at all times adhere to the best practices and values of the college.</li>
+    <li>The NSS unit in the college shall take all due measures to ensure that any and all projects undertaken and/or initiated by the university are successfully completed.</li>
 </ul>
        </div>
        <div class="nss-methodology">
@@ -129,9 +129,10 @@
         <ul class="list-unstyled p-0 list-icon-4 ps-4 mt-3">
             <li>The working culture of the NSS unit reflects respect for the individual and dignity of labor.</li>
             <li>An internal democratic process is established and pursued for choosing, planning, and implementing all programs by the NSS unit.</li>
-            <li>Training programs through workshops, community meetings, etc., for capacity building are a regular feature at both the university and community levels.</li>
+            <li>Training programs through workshops, community meetings, etc., for capacity building are a regular feature at both the college and community levels.</li>
             <li>There is interaction between student volunteers, various sections and classes of society to develop synergy for the conceptualization and implementation of activities/programs/camps at the district and possibly state levels as well.</li>
         </ul>
+       </div>
        </div>
        </div>
 
